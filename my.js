cf52b6f89a0fbb1cbe56580a82ad2cd6467cc353
botServic <82590694+botServic@users.noreply.github.com>
2021-12-24 06:33:30 +0300

    Update my.js

--- my.js
+++ my.js
@@ -1,6 +1,6 @@
 function testGS(){
 
-const url = "https://script.google.com/macros/s/AKfycbxwiCpNXhn58UVJhAYzspnZOAzCwgr49fJjELfm9hc1BFIRNHplQa_D6F3N3OQHbzg/exec"
+const url = "https://script.google.com/macros/s/AKfycbz7I27_U1dcgnOMd48Pg8ltyWgphA1MxV8gSgII2lL2h_N1F1kZzFLDLXmBi7Ync-Ixhg/exec"
 
 fetch(url)
     .then(d => d.json())
@@ -9,4 +9,23 @@ fetch(url)
     });
 }
 
+function addGS(){
+
+    const url = "https://script.google.com/macros/s/AKfycbz7I27_U1dcgnOMd48Pg8ltyWgphA1MxV8gSgII2lL2h_N1F1kZzFLDLXmBi7Ync-Ixhg/exec"
+    
+    fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        body: JSON.stringify({name:"Virtalik"}) // body data type must match "Content-Type" header
+      });
+        
+    }
+
 document.getElementById("btn").addEventListener("click",testGS);
+document.getElementById("btn2").addEventListener("click",addGS);
