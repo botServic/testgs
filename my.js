34de18321bdc63a85589d48590498e9038fc15bb
botServic <82590694+botServic@users.noreply.github.com>
2021-12-28 17:43:27 +0300

    Update my.js

--- my.js
+++ my.js
@@ -1,31 +1,27 @@
 function testGS(){
-
-const url = "https://script.google.com/macros/s/AKfycbz7I27_U1dcgnOMd48Pg8ltyWgphA1MxV8gSgII2lL2h_N1F1kZzFLDLXmBi7Ync-Ixhg/exec"
+var url = "https://script.google.com/macros/s/AKfycbxloPIyy6whKzduymK3OE1M4ZY76mIACSICNaiY8rhrIKDUO8_5kO2MyKQS5tLMM6A3pw/exec"
 
 fetch(url)
     .then(d => d.json())
-    .then(d =>{
-        document.getElementById("app").textContent = d[0].status;
+    .then(d => {
+        document.getElementById("app").textContent = d[0].status
     });
 }
 
 function addGS(){
-
-    const url = "https://script.google.com/macros/s/AKfycbz7I27_U1dcgnOMd48Pg8ltyWgphA1MxV8gSgII2lL2h_N1F1kZzFLDLXmBi7Ync-Ixhg/exec"
+    var url = "https://script.google.com/macros/s/AKfycbxloPIyy6whKzduymK3OE1M4ZY76mIACSICNaiY8rhrIKDUO8_5kO2MyKQS5tLMM6A3pw/exec"
     
     fetch(url, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'no-cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        headers: {
-          'Content-Type': 'application/json'
-          // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: 'follow', // manual, *follow, error
-        body: JSON.stringify({name:"Virtalik"}) // body data type must match "Content-Type" header
-      });
-        
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'no-cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    body: JSON.stringify({name:"Virtalik"}) // body data type must match "Content-Type" header
+    });
     }
-
-document.getElementById("btn").addEventListener("click",testGS);
 document.getElementById("btn2").addEventListener("click",addGS);
+document.getElementById("btn").addEventListener("click",testGS);
